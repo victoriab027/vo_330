05a046b7cde6be557cfb63bcc62942732535b5a2
PontifexMaximus268 <omanreinhart@gmail.com>
2023-01-19 16:51:34 -0600

    files search and file type and separate display implemented

--- M2/files.php
+++ M2/files.php
@@ -18,41 +18,94 @@ $dir = '/srv/uploads/' . $username;
 // Scan the directory and get all the files
 $files = scandir($dir);
 ?>
-<h3>Your Files</h3>
+<h3>Search Your Files</h3>
 <form>
-  Search: <input type="text" name="search"><br><br>
+  Search: <input type="text" name="search">
+  <label for="fileType">Filter File Type:</label>
+  <select id="fileType" name="fileType">
+    <option value="ALL">All</option>
+    <option value="TXT">Text (TXT)</option>
+    <option value="JPG">Images (JPG)</option>
+    <option value="PNG">Images (PNG)</option>
+    <option value="GIF">Images (GIF)</option>
+    <option value="PDF">Documents (PDF)</option>
+    <option value="DOC">Documents (DOC)</option>
+    <option value="DOCX">Documents (DOCX)</option>
+    <option value="MP3">Audio (MP3)</option>
+    <option value="WAV">Audio (WAV)</option>
+    <option value="AAC">Audio (AAC)</option>
+    <option value="MP4">Video (MP4)</option>
+  </select>
+  <button>Search</button>
+  <br><br>
 </form>
 <?php
 // Get the search value from the input field
+$selectedFileType = $_GET['fileType'];
 $search = (string) $_GET['search'];
-if( !preg_match('/^[\w_\.\-]+$/', $search) ){
-  print_r('Inavlid Search Query:');
-}
-else if (empty($search)){
+
+if (empty($search) && $selectedFileType == "ALL"){
+  foreach ($files as $file) {
+    // Ignore . and .. directories
+    if ($file != '.' && $file != '..') {
+        echo $file . '<br>';
+    }
+  }
+} else if (empty($search)){
+  foreach ($files as $file) {
+    // Ignore . and .. directories
+    if ($file != '.' && $file != '..') {
+      if (stripos($file, $selectedFileType) !== false) {
+        echo $file . '<br>';
+    }
+  }
+  }
+}else if( !preg_match('/^[\w_\.\-]+$/', $search) ){
+  print_r('Invalid Search Query');
+} else if ($selectedFileType == "ALL"){
+  print_r('Search Query: '.$search);
+  print_r("<br>");
+
+  // Loop through the files and print them
   foreach ($files as $file) {
     // Ignore . and .. directories
     if ($file != '.' && $file != '..') {
       // Check if the file name contains the search value
+      if (stripos($file, $search) !== false) {
         echo $file . '<br>';
+      }
     }
   }
 } else{
-print_r('Search Query:'.$search);
-print_r("<br>");
+  print_r('Search Query: '.$search);
+  print_r("<br>");
 
+  // Loop through the files and print them
+  foreach ($files as $file) {
+    // Ignore . and .. directories
+    if ($file != '.' && $file != '..') {
+      // Check if the file name contains the search value
+      if (stripos($file, $search) !== false && stripos($file, $selectedFileType) !== false) {
+        echo $file . '<br>';
+      }
+    }
+  }
+}
+?>
+<h3>Your Files</h3>
+<?php
 // Loop through the files and print them
 foreach ($files as $file) {
   // Ignore . and .. directories
   if ($file != '.' && $file != '..') {
     // Check if the file name contains the search value
-    if (stripos($file, $search) !== false) {
+    
       echo $file . '<br>';
-    }
+    
   }
 }
-}
-?>
 
+?>
 <h3>View a File</h3>
 <form name = "view" method = "POST" action = "view.php">
     <select name = "view">
